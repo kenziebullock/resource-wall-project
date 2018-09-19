aa70fe0bfece9129d80222da83f0cd2611f87069
Cheng Shi <cshi@ualberta.ca>
2018-09-19 19:25:46 +0000

    add migration alter users table

---
+++ db/migrations/20180919192031_alter_users_structure.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('users', function(table) {
+    table.string('email');
+    table.unique('email');
+    table.string('password');
+    table.string('avatar');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('users', function(table) {
+    table.dropColumn('email');
+    table.dropColumn('password');
+    table.dropColumn('avatar');
+  })
+};
