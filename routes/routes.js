db6da9c2530f6d9e16574b97bc1246ba64739f05
Kenzie Bullock <kenzieb_13@shaw.ca>
2018-09-20 00:57:12 +0000

    working on registration routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,50 +1,36 @@
 const express = require('express');
 const app = express.Router();
-
+const func = require('../public/scripts/helper-functions');
+// const users = require('./users');
 
 // Home page
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-// app.use(function(req, res, next) {
+app.get("/index", (req, res) => {
+    res.render("index");
+});
   
-//   // log each request to the console
-//   console.log(req.method, req.url);
-
-//   // continue doing what we were doing and go to the route
-//   next(); 
-// });
-
 
 // Login Page
-
 app.route('/login')
-    
-  .get(function(req, res) {
-  // const templateVars = {
-  //   email: req.session.email,
-  //   username: req.session.username
-  // }
-  // if (req.session.authenticated) {
-  //   res.redirect('/');
-  // }
-  res.render('index');
+  .get((req, res) => {
+    func.loginCheck(req, res);
+    res.render('login', func.templateVars);
   }) 
 
-  .post(function(req, res) {
-  // const users = db.users;
-  if (req.body.email === 'alice@gmail.com' && req.body.password === 'alice') {
-    req.session.email = req.body.email;
-    res.redirect('/');
-  } else {
-    res.redirect('/login');
-  }
-
+  .post((req, res) => {
+    // func.loginCheck(req, res);
+    func.errorCheck(req, res);
+    func.userAuthentication(req, res);
+    res.render('')
   });
 
+// logout current user
 app.route('/logout')
-  .get(function(req, res) {
+  .get((req, res) => {
+    req.session.email = null;
     delete req.session;
     res.redirect('/login');
   });
@@ -52,76 +38,80 @@ app.route('/logout')
 // Registration Page
 
 app.route('/register')
-  .get(function(req, res) {
-    res.send('register get route');
+  .get((req, res) => {
+    func.loginCheck(req, res);
+    res.render('register');
   })
-  .post(function(req, res) {
+  .post((req, res) => {
+    // registration
+    func.errorCheck(req, res);
+
+
     res.send('register post route');
   });
-
-
 
 //  New Resource Page
 
 app.route('/resources/new')
-  .get(function(req, res) {
+  .get((req, res) => {
     res.send('resources/new get route');
   })
-  .post(function(req, res) {
+  .post((req, res) => {
     res.send('resources/new post route');
   });
 
 // View all resources
 
 app.route('/resources')
-  .get(function(req, res) {
+  .get((req, res) => {
     res.send('resources get route');
   })
 
 // View specific resource
 
 app.route('/resources/:id')
-  .get(function(req, res) {
+  .get((req, res) => {
     res.send('resources/:id get route');
   })
 
 // Comment/Like/Rate specific resource
 
 app.route('/resources/:id/comment')
-  .post(function(req, res) {
+  .post((req, res) => {
     res.send('resources/:id/comment post route');
   })
 
 app.route('/resources/:id/rate')
-  .post(function(req, res) {
+  .post((req, res) => {
     res.send('resources/:id/rate post route');
   })
 
 app.route('/resources/:id/like')
-  .post(function(req, res) {
+  .post((req, res) => {
     res.send('resources/:id/like post route');
   })
 
 // User profile page
 
 app.route('/users/:id')
-  .get(function(req, res) {
+  .get((req, res) => {
     res.send('users/:id get route');
   })
 
 // User update/edit page
 
 app.route('/users/:id/update')
-  .get(function(req, res) {
+  .get((req, res) => {
     res.send('users/:id/update get route');
   })
-  .post(function(req, res) {
+  .post((req, res) => {
     res.send('users/:id/update post route');
   })
 
 app.route('/users/:id/resources')
-  .get(function(req, res) {
+  .get((req, res) => {
     res.send('/users/:id/resources');
   })
 
 module.exports = app;
+
